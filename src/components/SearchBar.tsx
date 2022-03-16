8bca7108facc32d788d478965f5bbe2fbf88c1a4
Alp Tarla <alptarla@gmail.com>
2022-03-16 23:58:40 +0300

    Add clear filters button

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,34 +1,36 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent } from "react";
 import { Form, Stack } from "react-bootstrap";
 
 interface Props {
   onSearch: (search: string) => void;
   onAvailableSelected: (isAvailable: boolean) => void;
+  searchValue: string;
+  isAvailableValue?: boolean;
 }
 
-function SearchBar({ onSearch, onAvailableSelected }: Props) {
-  const [search, setSearch] = useState("");
-  const [isAvailable, setIsAvailable] = useState(false);
-
+function SearchBar({
+  onSearch,
+  onAvailableSelected,
+  searchValue,
+  isAvailableValue,
+}: Props) {
   const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
     onSearch(e.target.value);
   };
 
   const handleAvailableChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setIsAvailable(e.target.checked);
     onAvailableSelected(e.target.checked);
   };
 
   return (
     <Stack className="gap-2 mb-3 w-50 mx-auto">
       <Form.Control
-        value={search}
+        value={searchValue}
         onChange={handleSearchChange}
         placeholder="Search movies..."
       />
       <Form.Check
-        checked={isAvailable}
+        checked={isAvailableValue}
         onChange={handleAvailableChange}
         label="Show available movies"
       />
--- src/components/movie/MovieFilter.tsx
+++ src/components/movie/MovieFilter.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Stack } from "react-bootstrap";
+import { Button, Stack } from "react-bootstrap";
 import { Genre } from "../../services/genre-service";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { getGenres } from "../../store/slices/genre-slice";
@@ -8,12 +8,14 @@ import GenreList from "../GenreList";
 import { GenreListLoader } from "../loaders";
 import SearchBar from "../SearchBar";
 
+const DEFAULT_FILTERS = {
+  search: "",
+  isAvailable: false,
+  genres: [],
+};
+
 function MovieFilter() {
-  const [filters, setFilters] = useState<Filters>({
-    search: "",
-    isAvailable: false,
-    genres: [],
-  });
+  const [filters, setFilters] = useState<Filters>(DEFAULT_FILTERS);
 
   const { genres, status } = useAppSelector((state) => state.genre);
   const { filters: storeFilters } = useAppSelector((state) => state.movie);
@@ -23,6 +25,14 @@ function MovieFilter() {
   useEffect(() => {
     dispatch(getGenres());
   }, [dispatch]);
+
+  useEffect(() => {
+    // ** not dispatch changes to store, if filters same to filters into store
+    if (JSON.stringify(filters) === JSON.stringify(storeFilters)) {
+      return;
+    }
+    dispatch(updateFilters(filters));
+  }, [filters, dispatch, storeFilters]);
 
   const handleSearch = (search: string) => {
     setFilters((prev) => ({ ...prev, search }));
@@ -51,17 +61,28 @@ function MovieFilter() {
     });
   };
 
-  useEffect(() => {
-    // ** not dispatch changes to store, if filters same to filters into store
-    if (JSON.stringify(filters) === JSON.stringify(storeFilters)) {
-      return;
-    }
-    dispatch(updateFilters(filters));
-  }, [filters, dispatch, storeFilters]);
+  const clearFilters = () => {
+    dispatch(updateFilters(DEFAULT_FILTERS));
+    setFilters(DEFAULT_FILTERS);
+  };
+
+  const isClearButtonDisabled =
+    JSON.stringify(filters) === JSON.stringify(DEFAULT_FILTERS);
 
   return (
-    <Stack className="border rounded p-3 mb-3 bg-light shadow-sm">
+    <Stack className="border rounded p-3 mb-3 bg-light shadow-sm position-relative">
+      <Button
+        size="sm"
+        variant="warning"
+        className="position-absolute top-0 end-0 my-1 mx-2"
+        onClick={clearFilters}
+        disabled={isClearButtonDisabled}
+      >
+        Clear filters
+      </Button>
       <SearchBar
+        searchValue={filters.search}
+        isAvailableValue={filters.isAvailable}
         onSearch={handleSearch}
         onAvailableSelected={handleAvailable}
       />
